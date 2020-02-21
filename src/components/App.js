517977e4a1441fcfee05e37cb6244f9c49ef22d4
Ben Beekman <bbeekman@gmail.com>
2020-02-21 11:21:49 -0800

    header nav pair programmed with Mike

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './commons/Header/Header';
+import Header from './Header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return <Header />;
