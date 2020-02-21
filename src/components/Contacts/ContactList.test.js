1ed2d315b3dd1067a39916f263724807ca143e9b
drmeloy <drmeloy@gmail.com>
2020-02-21 15:48:15 -0800

    wrote ContactList snapshot test, awaiting store.js

---
+++ src/components/Contacts/ContactList.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ContactList from './ContactList';
+import { Provider } from 'react-redux';
+import { store } from '../../store';
+
+describe('ContactList component', () => {
+  it('matches snapshot', () => {
+    const wrapper = shallow(<Provider store={store}><ContactList /></Provider>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
