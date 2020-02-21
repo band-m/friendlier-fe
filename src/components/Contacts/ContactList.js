79eecdef3b15f35a2dfba2ed039898331eede60a
drmeloy <drmeloy@gmail.com>
2020-02-21 14:49:41 -0800

    adding commStatus equation to contact list

--- src/components/Contacts/ContactList.js
+++ src/components/Contacts/ContactList.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectContactsList } from '../../data/selectors/contacts-selectors';
+
 
 export default function ContactList(){
-  // get contacts from state
-  const contacts = getContacts();
+  const state = useSelector(state => state);
+  const contacts = selectContactsList(state);
+
   const contactList = contacts.map(contact => {
-    const statusIcon = goal/lastContact > 
+    const deadline = contact.commFreq - contact.lastContact;
+    const commStatus = 
+      deadline < contact.yellowZone ? 'green' :
+      deadline > contact.yellowZone && deadline < contact.redZone ? 'yellow' :
+      deadline > contact.redZone < 1 ? 'red' :
+      'overdue';
+      
 
     <Link to=''>
       <li key={contact.id}>
