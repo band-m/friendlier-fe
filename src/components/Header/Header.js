03625c8c9fd3d0909f1ea35bb99991fcee7a1975
Ben Beekman <bbeekman@gmail.com>
2020-02-21 14:40:22 -0800

    nested css and consistent class case

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './Header.css';
 import { Link } from 'react-router-dom';
 const Header = () => (
-  <header>
+  <header className={styles.Header}>
     <Link to="/">
-      <div className={styles.logo} alt="re:connect logo" title="Back to Home"></div>
+      <div className={styles.Logo} alt="re:connect logo" title="Back to Home"></div>
     </Link>
     <nav className={styles.Nav}>
       <Link to="/contacts"><span>Contact List</span></Link>
