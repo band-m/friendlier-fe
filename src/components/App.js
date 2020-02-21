09a006cfc64c133950173be8d584e747cb171f35
Ben Beekman <bbeekman@gmail.com>
2020-02-21 11:16:09 -0800

    header navigation pair programmed with Mike

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import MenuList from './commons/Header/Header';
+import Header from './commons/Header/Header';
 
-const menu = [
-  { label: 'Home', url: '#' },
-  { label: 'Contact List', url: '#' },
-  { label: 'Add Contact', url: '#' },
-  { label: 'About', url: '#' },
-  { label: 'Settings', url: '#' },
-  { label: 'Logout', url: '#' },
-];
 export default function App() {
-  return <MenuList menuItems={menu} />;
+  return <Header />;
 }
