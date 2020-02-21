9534fd49ee0add404d499f958950505272899f62
TravelFiend <mikegrace274@gmail.com>
2020-02-21 11:47:09 -0800

    paired with ben to set up router

--- src/components/App.js
+++ src/components/App.js
@@ -3,5 +3,16 @@ import Header from './Header/Header';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/about" component={About} />
+        <Route path="/settings" component={Settings} />
+      </Switch>
+    </Router>
+  )
 }
+
