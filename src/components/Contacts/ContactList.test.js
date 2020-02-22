2da7f8ac14219de992e8e068629328612b292ded
drmeloy <drmeloy@gmail.com>
2020-02-21 16:08:00 -0800

    fixed snapshot test

--- src/components/Contacts/ContactList.test.js
+++ src/components/Contacts/ContactList.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ContactList from './ContactList';
 import { Provider } from 'react-redux';
-import { store } from '../../store';
+import store from '../../store';
 
 describe('ContactList component', () => {
   it('matches snapshot', () => {
