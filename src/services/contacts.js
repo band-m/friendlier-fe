d29529322a47c6986f806f7e8deab642a9b03034
Ben Beekman <bbeekman@gmail.com>
2020-02-24 13:25:16 -0800

    Contact services (#24)
    * getContactDetails, updateContactDetails partially written
    
    * finished writing updateContactDetails and deleteContactDetails

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,3 +6,41 @@ export const getContacts = userId => {
       return data;
     });
 };
+
+export const getContactDetails = contactId => {
+  return fetch(`/api/v1/contacts/${contactId}`)
+    .then(res => Promise.all[res.ok, res.json()])
+    .then(([ok, data]) => {
+      if(!ok) throw data;
+      return data;
+    });
+};
+
+export const updateContactDetails = (contactId, body) => {
+  return fetch(`/api/v1/contacts/${contactId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+    .then(res => Promise.all[res.ok, res.json()])
+    .then(([ok, data]) => {
+      if(!ok) throw data;
+      return data;
+    });
+};
+
+export const deleteContactDetails = contactId => {
+  return fetch(`/api/v1/contacts/${contactId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => Promise.all[res.ok, res.json()])
+    .then(([ok, data]) => {
+      if(!ok) throw data;
+      return data;
+    });
+};
