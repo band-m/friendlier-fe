2d31cb312d2d0295e9a229050beb6b50ef9c4903
Nick <nickbrimmer@gmail.com>
2020-02-24 14:58:44 -0800

    cleaned up a small typo

--- src/data/actions/contact-detail-actions.js
+++ src/data/actions/contact-detail-actions.js
@@ -1,19 +1,5 @@
 import {
   FETCH_CONTACT_DETAILS,
-  SET_CONTACT_DETAILS,
-  SET_FIRST_NAME,
-  SET_LAST_NAME,
-  SET_PHONE_NUMBER,
-  SET_ADDRESS,
-  SET_EMAIL,
-  SET_IMAGE,
-  SET_COMM_FREQUENCY,
-  SET_BIRTHDATE,
-  SET_SPECIAL_DATES,
-  SET_NOTES,
-  SET_LAST_CONTACTED,
-  SET_YELLOW_ZONE,
-  SET_RED_ZONE
 } from '../action-types/action-types';
 import { getContactDetails } from '../../services/contacts';
 
