0d3939a1eaea4c4bda122ce3dd60b3d7c644674a
Ben Beekman <bbeekman@gmail.com>
2020-02-24 13:05:03 -0800

    getContactDetails, updateContactDetails partially written

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,3 +6,27 @@ export const getContacts = userId => {
       return data;
     });
 };
+
+export const getContactDetails = contactId => {
+  return fetch(`/api/v1/contacts/${contactId}`)
+    .then(res => Promise.all[res.ok, res.json()])
+    .then(([ok, data]) => {
+      if(!ok) throw data;
+      return data;
+    });
+};
+
+export const updateContactDetails = (contactId, body) => {
+  return fetch(`/api/v1/contacts/${contactId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body
+  });
+
+};
+
+//updateContactDetails
+//deleteContact
+//
