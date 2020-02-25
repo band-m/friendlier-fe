eb1ad15d698f002c44d8ff29f3b0248d62f429dd
drmeloy <drmeloy@gmail.com>
2020-02-25 09:46:08 -0800

    moved App.js back to prev config

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,19 @@ import Home from './Home/HomePage';
 import About from './About/AboutPage';
 import Contacts from './Contacts/ContactsPage';
 import Settings from './Settings/SettingsPage';
-import Slider from './Slider/Slider';
 
 export default function App() {
   return (
-    <Slider />
-    // <Router>
-    //   <Header />
-    //   <Switch>
-    //     <Route exact path="/" component={Home} />
-    //     <Route path="/contacts" component={Contacts} />
-    //     <Route path="/about" component={About} />
-    //     <Route path="/settings" component={Settings} />
-    //   </Switch>
-    //     {/* <Route path='/contacts/:id' component={ContactDetail} />
-    //     <Route path='/contacts/add' component={AddContact} /> */}
-    // </Router>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/about" component={About} />
+        <Route path="/settings" component={Settings} />
+      </Switch>
+        {/* <Route path='/contacts/:id' component={ContactDetail} />
+        <Route path='/contacts/add' component={AddContact} /> */}
+    </Router>
   );
 }
