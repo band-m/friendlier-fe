3a8b2756e5aee80abce08416fdb4a9df7d2d8499
Ben Beekman <bbeekman@gmail.com>
2020-02-24 16:33:37 -0800

    basic login form written and placed on HomePage component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import styles from './Login.css';
 
 const Login = () => {
 
   const [input, setInput] = useState();
   const [password, setPassword] = useState();
-  
-
 
   return (
-    <>
-
-    </>
+    <main className={styles.Login}>
+      <h1>Login/Signup</h1>
+      <label>Email<input type="email" name="email" /></label>
+      <label>Password<input type="password" name="password" /></label>
+      <button>Login</button>
+    </main>
   );
 };
 
