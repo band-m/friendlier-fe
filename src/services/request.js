7afc9882882b17da54d77876dcf60d16d0fc2ee4
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-25 11:27:45 -0800

    added request function

---
+++ src/services/request.js
@@ -0,0 +1,15 @@
+export default (path, method, body) => {
+  return fetch(`${process.env.API_URL}${path}`,{
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: body && JSON.stringify(body)
+  })
+    .then(res => Promise.all([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json;
+    });
+};
