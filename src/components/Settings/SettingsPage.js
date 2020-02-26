80f700e035a5fd4a9a37b74041c6ccd46062960c
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-26 15:49:52 -0800

    saveSettings function

--- src/components/Settings/SettingsPage.js
+++ src/components/Settings/SettingsPage.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
 import styles from './SettingsPage.css';
+import { useDispatch } from 'react-redux';
+import { updateUser } from '../../data/actions/auth-actions';
+import registerPush from '../../workers/register-push';
 
 const SettingsPage = () => {
   const [password, setPassword] = useState('');
-  const [pushesOn, setPushesOn] = useState(false);
+  const [wantsPush, setWantsPush] = useState(false);
   const [pushHour, setPushHour] = useState(6);
   const [pushIsPM, setPushIsPM] = useState(false);
+  const dispatch = useDispatch();
   const hours = Array(12).fill().map((x, i)=>i);
+  const saveSettings = () => {
+    dispatch(updateUser({
+      pushHour: pushHour + 12 * Number(pushIsPM),
+      wantsPush
+    }));
+    if(wantsPush) {
+      registerPush();
+    }
+    else {
+      // would be nice to be able to unregister
+    }
+  };
 
   return (
     <main className={styles.SettingsPage}>
@@ -15,8 +31,8 @@ const SettingsPage = () => {
       <div>
         Receive daily notifications: 
         <input type="checkbox" 
-          value={pushesOn} 
-          onChange={({ target }) => setPushesOn(target.value)} />
+          value={wantsPush} 
+          onChange={({ target }) => setWantsPush(target.value)} />
       </div>
       <div>
         At: <select value={pushHour} onChange={({ target }) => setPushHour(target.value) }>
@@ -32,7 +48,7 @@ const SettingsPage = () => {
       <div><label htmlFor="username">Username</label></div>
       <h2>Change Password</h2>
       <div><label htmlFor="password">Password</label><input type="password" name="password" value={password} onChange={({ target }) => setPassword(target.value)} /></div>
-      <button>Save Settings</button>
+      <button onClick={saveSettings}>Save Settings</button>
     </main>
   );
 };
