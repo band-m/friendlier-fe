548f24206db6c1537e55a9162bf577adeeefb643
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-26 12:57:33 -0800

    fixed scope

--- src/workers/client.js
+++ src/workers/client.js
@@ -13,7 +13,7 @@ else {
 
 async function run() {
   console.log('Registering service worker');
-  const registration = await navigator.serviceWorker.register('/workers/worker.js', { scope: '/' })
+  const registration = await navigator.serviceWorker.register('/src/workers/worker.js', { scope: '/src/workers/' })
     .then(registration);
   console.log('Registered service worker');
 
