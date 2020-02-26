db3ce037db6c20b07d132999c444093d51cd1bdd
Dan Meloy <drmeloy@gmail.com>
2020-02-25 16:23:11 -0800

    Connect app (#41)
    * beginning connecting the dots
    
    * added loading spinner
    
    * contact list page rendering
    
    * add contact page renderable
    
    * got tests passing

--- src/components/Contacts/ContactList.js
+++ src/components/Contacts/ContactList.js
@@ -5,30 +5,34 @@ import { selectContactsList } from '../../data/selectors/contacts-selectors';
 
 export default function ContactList() {
   const contacts = useSelector(selectContactsList);
-
-  const contactList = contacts.map(contact => {
-    const ratio = 1 - ((contact.commFreq - contact.lastContact) / contact.commFreq);
-    const commStatus =
-      ratio < contact.yellowZone ? 'green' :
-        ratio > contact.yellowZone && ratio < contact.redZone ? 'yellow' :
-          ratio > contact.redZone < 1 ? 'red' :
-            'overdue';
-    return (
-      <Link key={contact._id} to={`/contacts/${contact._id}`}>
-        <li className={commStatus}>
-          <span>{contact.firstName} {contact.lastName}</span>
-          {/* <span>{statusIcon}</span> */}
-        </li>
-      </Link>);
-  });
+  
+  let contactList;
+  if(contacts.length > 0){
+    contactList = contacts.map(contact => {
+      const ratio = 1 - ((contact.commFreq - contact.lastContact) / contact.commFreq);
+      const commStatus =
+        ratio < contact.yellowZone ? 'green' :
+          ratio > contact.yellowZone && ratio < contact.redZone ? 'yellow' :
+            ratio > contact.redZone < 1 ? 'red' :
+              'overdue';
+      return (
+        <Link key={contact._id} to={`/contacts/${contact._id}`}>
+          <li className={commStatus}>
+            <span>{contact.firstName} {contact.lastName}</span>
+            {/* <span>{statusIcon}</span> */}
+          </li>
+        </Link>);
+    });
+  }
 
   return (
     <>
-      <ul>
+      {contactList && <ul>
         {contactList}
-      </ul>
-      <Link to='contacts/add'>
-        <img src='PLUS_ICON'></img>
+      </ul> }
+      {!contactList && <p>Add your first contact to get started!</p>}
+      <Link to='/add'>
+        <p>+</p>
       </Link>
     </>
   );
