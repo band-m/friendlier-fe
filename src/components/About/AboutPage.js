68212866655f47ca4c9f338f2e8e56c0c11a40d7
Ben Beekman <bbeekman@gmail.com>
2020-02-26 15:03:35 -0800

    minor copy change for about page

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -5,7 +5,7 @@ const AboutPage = () => {
   return (
     <main className={styles.About}>
       <h1>About KIT</h1>
-      <p>KIT is a tool for improving your communication with the people that matter to you. If you&apos;ve got personal or professional connections that you want to nourish with regular contact, re:connect can help.</p>
+      <p>KIT is a tool for improving your communication with the people that matter to you. If you&apos;ve got personal or professional connections that you want to nourish with regular contact, KIT can help.</p>
       <h2>Features include:</h2>
       <ul>
         <li>Fast and flexible communication scheduling</li>
@@ -14,7 +14,6 @@ const AboutPage = () => {
         <li>A user interface designed for efficiency</li>
       </ul>
       <p>Track birthdays, family check-ins, professional contacts, and friends with ease, and never again worry about neglecting the connections that you care about.</p>
-
     </main>
   );
 };
