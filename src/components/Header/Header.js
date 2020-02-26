e7e873c5b3d36df3acdb1c21ad91c2409b1f4792
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-26 11:01:25 -0800

    get user in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './Header.css';
 import { Link } from 'react-router-dom';
-const Header = () => (
-  <header className={styles.Header}>
-    <Link to="/">
-      <div className={styles.Logo} alt="re:connect logo" title="Back to Home"></div>
-    </Link>
-    <nav className={styles.Nav}>
-      <Link to="/contacts"><span>Contact List</span></Link>
-      <Link to="/about"><span>About</span></Link>
-      <Link to="/settings"><span>Settings</span></Link>
-      {/* <Link to="#"><span>Logout</span></Link> */}
-    </nav>
-  </header>
-);
+import { useSelector } from 'react-redux';
+import selectUser from '../../data/selectors/auth-selector';
+
+const Header = () => {
+  const user = useSelector(selectUser);
+  return (
+    <header className={styles.Header}>
+      <Link to="/">
+        <div className={styles.Logo} alt="re:connect logo" title="Back to Home"></div>
+      </Link>
+      <nav className={styles.Nav}>
+        <Link to="/contacts"><span>Contact List</span></Link>
+        <Link to="/about"><span>About</span></Link>
+        <Link to="/settings"><span>Settings</span></Link>
+        {/* <Link to="#"><span>Logout</span></Link> */}
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
