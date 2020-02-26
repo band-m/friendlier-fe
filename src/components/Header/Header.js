da1df19cde2e43b4c4444075af9e05c8b8b0f199
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-26 11:17:36 -0800

    selectUser is non-default export

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import styles from './Header.css';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import selectUser from '../../data/selectors/auth-selector';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectUser } from '../../data/selectors/auth-selector';
+import { logout } from '../../data/actions/auth-actions';
 
 const Header = () => {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const handleLogoutClick = () => dispatch(logout());
+
   return (
     <header className={styles.Header}>
       <Link to="/">
         <div className={styles.Logo} alt="re:connect logo" title="Back to Home"></div>
       </Link>
       <nav className={styles.Nav}>
-        <Link to="/contacts"><span>Contact List</span></Link>
+        {user && <Link to="/contacts"><span>Contact List</span></Link>}
         <Link to="/about"><span>About</span></Link>
-        <Link to="/settings"><span>Settings</span></Link>
-        {/* <Link to="#"><span>Logout</span></Link> */}
+        {user && <Link to="/settings"><span>Settings</span></Link>}
+        {user && <span onClick={handleLogoutClick}>Logout</span>}
       </nav>
     </header>
   );
