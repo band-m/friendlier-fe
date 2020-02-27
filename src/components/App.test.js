4a0200e51c73dd09033d6ddcb9f28226e18aa8e7
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-26 20:39:30 -0800

    silly ali, mocks aree for tests.

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,6 +4,8 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 jest.mock('../services/request.js');
+jest.mock('../workers/subscribe-push.js');
+jest.mock('../workers/unsubscribe-push.js');
 
 describe('App component', () => {
   it('renders App', () => {
