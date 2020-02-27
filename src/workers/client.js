4f1683feae00a101cd16df05e5cae455b69a8544
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-26 16:13:49 -0800

    cleanup

--- src/workers/client.js
+++ src/workers/client.js
@@ -1,6 +1,3 @@
-import urlBase64ToUint8Array from './urlBase64ToUint8Array';
-const publicVapidKey = 'BBDk-M7Qx45gAgYzwp3epWc_PK7Iv4cF9kehNJ0YWbdLinh8JFVUhPgvoq2fQfQYLkqKISqc55D1z5Shnqk6M8A';
-
 if('serviceWorker' in navigator) {
   console.log('Registering service worker');
 
--- src/workers/register-push.js
+++ src/workers/register-push.js
@@ -2,9 +2,9 @@ import urlBase64ToUint8Array from './urlBase64ToUint8Array';
 const publicVapidKey = 'BBDk-M7Qx45gAgYzwp3epWc_PK7Iv4cF9kehNJ0YWbdLinh8JFVUhPgvoq2fQfQYLkqKISqc55D1z5Shnqk6M8A';
 
 export default async() => {
-  const registration = await navigator.serviceWorker.getRegistration();
+  const registration = await navigator.serviceWorker.getRegistration('/src/workers/worker.js');
   console.log('Registering push');
-  const subscription = await registration.pushManager.subscribe({
+  await registration.pushManager.subscribe({
     userVisibleOnly: true,
     applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
   });
