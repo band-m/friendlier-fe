4e1db030a2540c3a77a9c1012026ace5dabda1f2
TravelFiend <mikegrace274@gmail.com>
2020-02-27 17:09:17 -0800

    sorting theoretically working on contactList

--- src/components/Contacts/ContactList.js
+++ src/components/Contacts/ContactList.js
@@ -8,7 +8,8 @@ import isPast from 'date-fns/isPast';
 import differenceInCalendarDays from 'date-fns/differenceInCalendarDays';
 import { selectUser, selectLoggedOut } from '../../data/selectors/auth-selector';
 import styles from './ContactList.css';
-import { SET_CONTACT_CREATED_ON } from '../../data/action-types/action-types';
+import { SET_CONTACT_CREATED_ON, SET_RED_ZONE, SET_CURRENT_ZONE_RATIO } from '../../data/action-types/action-types';
+import { myAction } from '../../data/actions/contact-detail-actions';
 
 export default function ContactList() {
   const dispatch = useDispatch();
@@ -29,80 +30,91 @@ export default function ContactList() {
 
   const contacts = useSelector(selectContactsList);
 
-  // const greenZoneContacts = contacts.filter(contact => isFuture(new Date(contact.yellowZoneStartDate)));
-  // const yellowZoneContacts = contacts.filter(contact => isFuture(new Date(contact.redZoneStartDate)) && isPast(new Date(contact.yellowZoneStartDate)));
-  // const redZoneContacts = contacts.filter(contact => isFuture(new Date(contact.deadlineDate)) && isPast(new Date(redZoneStartDate)));
-  // const overdueContacts = contacts.filter(contact => isPast(new Date(contact.deadlineDate)));
+  const greenZoneContacts = contacts.filter(contact => isFuture(new Date(contact.yellowZoneStartDate)));
+  const yellowZoneContacts = contacts.filter(contact => isFuture(new Date(contact.redZoneStartDate)) && isPast(new Date(contact.yellowZoneStartDate)));
+  const redZoneContacts = contacts.filter(contact => isFuture(new Date(contact.deadlineDate)) && isPast(new Date(contact.redZoneStartDate)));
+  const overdueContacts = contacts.filter(contact => isPast(new Date(contact.redZoneStartDate)));
 
-  // const getRatio = (contacts, zoneDays) => {
-  //   return contacts.map(contact => {
-  //     const daysIntoZone = contact.lastContactedDate ?
-  //       differenceInCalendarDays(contact.lastContactedDate, Date.now()) :
-  //       differenceInCalendarDays(contact.createdOn, Date.now());
-  //     const ratio = daysIntoZone / zoneDays;
-  //   });
-  // };
-
-  // SET_CURRENT_ZONE_RATIO
-
-  // yellowZoneContacts.map(contact => {
-  //   const daysIntoZone = contact.lastContactedDate ? 
-  //     differenceInCalendarDays(contact.lastContactedDate, Date.now()) :
-  //     differenceInCalendarDays(contact.createdOn, Date.now());
-  //   const ratio = daysIntoZone / contact.totalYellowZoneDays;
-  // });
-
-  // redZoneContacts.map(contact => {
-  //   const daysIntoZone = contact.lastContactedDate ? 
-  //     differenceInCalendarDays(contact.lastContactedDate, Date.now()) :
-  //     differenceInCalendarDays(contact.createdOn, Date.now());
-  //   const ratio = daysIntoZone / contact.totalRedZoneDays;
-  // });
-
-  // const sortedGreenZoneContacts = greenZoneContacts.sort((a, b) => {
-  //     return new Date(b.)
-  // });
-
-  // const makeListItems = zone => {
-  //   if(zone.length){
-  //     return zone.map(contact => {
-  //       return (
-  //         <Link key={contact._id} to={`/contacts/${contact._id}`}>
-  //           <li className={styles.commStatus}>
-  //             <span>{contact.firstName} {contact.lastName}</span>
-  //             {/* <span>{statusIcon}</span> */}
-  //           </li>
-  //         </Link>
-  //       );
-  //     });
-  //   }
-  // };
-
-  // let greenZoners = makeListItems(sortedGreenZoneContacts);
-  // let redZoners = makeListItems(sortedYellowZoneContacts);
-  // let yellowZoners = makeListItems(sortedRedZoneContacts);
-  // let pastDeadlineZoners = makeListItems(sortedPastDeadlineContacts);
-
-
-  let contactList;
-  if(contacts.length) {
-    contactList = contacts.map(contact => {
-      return (
-        <Link key={contact._id} to={`/contacts/${contact._id}`}>
-          <li className={styles.commStatus}>
-            <span>{contact.firstName} {contact.lastName}</span>
-            {/* <span>{statusIcon}</span> */}
-          </li>
-        </Link>);
+  useEffect(() => {
+    greenZoneContacts.map(contact => {
+      const daysIntoZone = contact.lastContactedDate ?
+        differenceInCalendarDays(contact.lastContactedDate, Date.now()) :
+        differenceInCalendarDays(contact.createdOn, Date.now());
+      const ratio = daysIntoZone / contact.totalGreenZoneDays;
+      dispatch(myAction(SET_CURRENT_ZONE_RATIO, ratio));
     });
-  }
+
+    yellowZoneContacts.map(contact => {
+      const daysIntoZone = contact.lastContactedDate ?
+        differenceInCalendarDays(contact.yellowZoneStartDate, Date.now()) :
+        differenceInCalendarDays(contact.yellowZoneStartDate, Date.now());
+      const ratio = daysIntoZone / contact.totalYellowZoneDays;
+      dispatch(myAction(SET_CURRENT_ZONE_RATIO, ratio));
+    });
+
+    redZoneContacts.map(contact => {
+      const daysIntoZone = contact.lastContactedDate ?
+        differenceInCalendarDays(contact.redZoneStartDate, Date.now()) :
+        differenceInCalendarDays(contact.redZoneStartDate, Date.now());
+      const ratio = daysIntoZone / contact.totalRedZoneDays;
+      dispatch(myAction(SET_CURRENT_ZONE_RATIO, ratio));
+    });
+  }, []);
+
+  const sortedGreen = greenZoneContacts.sort((a, b) => {
+    a.zoneRatio - b.zoneRatio;
+  });
+
+  const sortedYellow = yellowZoneContacts.sort((a, b) => {
+    a.zoneRatio - b.zoneRatio;
+  });
+
+  const sortedRed = redZoneContacts.sort((a, b) => {
+    a.zoneRatio - b.zoneRatio;
+  });
+
+  const makeListItems = zone => {
+    if(zone.length){
+      return zone.map(contact => {
+        return (
+          <Link key={contact._id} to={`/contacts/${contact._id}`}>
+            <li className={styles.commStatus}>
+              <span>{contact.firstName} {contact.lastName}</span>
+              {/* <span>{statusIcon}</span> */}
+            </li>
+          </Link>
+        );
+      });
+    }
+  };
+
+  let greenZoners = makeListItems(sortedGreen);
+  let redZoners = makeListItems(sortedYellow);
+  let yellowZoners = makeListItems(sortedRed);
+  let overdueZoners = makeListItems(overdueContacts);
+
+  // let contactList;
+  // if(contacts.length) {
+  //   contactList = contacts.map(contact => {
+  //     return (
+  //       <Link key={contact._id} to={`/contacts/${contact._id}`}>
+  //         <li className={styles.commStatus}>
+  //           <span>{contact.firstName} {contact.lastName}</span>
+  //           {/* <span>{statusIcon}</span> */}
+  //         </li>
+  //       </Link>);
+  //   });
+  // }
 
   return (
     <section className={styles.ContactList}>
-      {contactList && <ul>
-        {contactList}
+      {contacts && <ul>
+        {greenZoners}
+        {redZoners}
+        {yellowZoners}
+        {overdueZoners}
       </ul>}
-      {!contactList && <p>Add your first contact to get started!</p>}
+      {!contacts && <p>Add your first contact to get started!</p>}
       <Link to='/add'>
         <button title="Add New Contact">+</button>
       </Link>
