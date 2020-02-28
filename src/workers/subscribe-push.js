4c721f6db58c93988c64dcd4ed3e706849d753f3
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-27 17:33:38 -0800

    oops.

--- src/workers/subscribe-push.js
+++ src/workers/subscribe-push.js
@@ -2,7 +2,7 @@ import urlBase64ToUint8Array from './urlBase64ToUint8Array';
 const publicVapidKey = 'BBDk-M7Qx45gAgYzwp3epWc_PK7Iv4cF9kehNJ0YWbdLinh8JFVUhPgvoq2fQfQYLkqKISqc55D1z5Shnqk6M8A';
 
 export default async() => {
-  const registration = await navigator.serviceWorker.getRegistration('/src/workers/worker.js');
+  const registration = await navigator.serviceWorker.getRegistration('/worker.js');
   console.log('Registering push');
   return registration.pushManager.subscribe({
     userVisibleOnly: true,
